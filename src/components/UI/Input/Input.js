df7fbaacb3f488a813a7c0660748938ba4a73a5f
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-31 00:31:15 -0700

    implement forwardRef

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useRef, useImperativeHandle } from "react";
 import classes from "./input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate
+    };
+  });
+
   return (
     <div
       className={`${classes.control} ${
@@ -15,9 +27,10 @@ const Input = (props) => {
         value={props.value}
         onChange={props.onChange}
         onBlur={props.onBlur}
+        ref={inputRef}
       />
     </div>
   );
-};
+});
 
 export default Input;
